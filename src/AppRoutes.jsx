25482185ad342fdc8b2a730dc15595e35262efd3
Arinze Ogbuniba <niklaus.oa@gmail.com>
2024-07-06 13:07:03 +0100

    Initial commit

---
+++ src/AppRoutes.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import Checkout from "./pages/Checkout";
+
+
+const AppRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+
+const Header = () => {
+  return (
+    <div>
+      <nav className="p-4">
+        <ul className="flex items-center gap-4 text-blue-700 font-bold">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="cart">Cart</Link></li>
+          <li><Link to="checkout">Checkout</Link></li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
