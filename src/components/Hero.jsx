a8dececb3bffaab2e3abb2066afece12ffb40b8f
Arinze Ogbuniba <niklaus.oa@gmail.com>
2024-07-07 20:48:53 +0100

    Initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import { RiArrowRightLine } from "react-icons/ri";
+import { IKImage } from "imagekitio-react";
+
+const urlEndpoint = "https://ik.imagekit.io/kushyzee/timbucloudshop";
+const Hero = () => {
+  return (
+    <section>
+        
+    </section>
+  );
+};
+
+export default Hero;
