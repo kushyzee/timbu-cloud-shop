e44949db0d58cc7ecc9ed634caca0364f98c98bc
niklaus.oa <niklaus.oa@gmail.com>
2024-07-08 15:26:39 +0000

    adjust min-width to 350px

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '../Header'
 import Footer from '../Footer'
 
 const Layout = () => {
+
   return (
-    <div className='bg-customGray-50 min-w-[340px]'>
+    <div className='bg-customGray-50 min-w-[350px]'>
       <Header />
       <main className='px-5'>
         <Outlet />
