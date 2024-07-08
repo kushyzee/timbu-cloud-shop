c9fb461b53757532a945bd629900347ce38b04e2
niklaus.oa <niklaus.oa@gmail.com>
2024-07-08 07:07:32 +0000

    Format code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,14 @@ import { MdOutlineShoppingCart } from "react-icons/md";
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center p-4 bg-black text-white">
-      <p className="text-sm font-bold">TimbuCloudShop</p>
+    <header className="flex justify-between items-center p-4 bg-customGray-800 mb-6 text-white">
+      <Link to="/" className="text-sm font-bold">
+        TimbuCloudShop
+      </Link>
       <Link to="cart">
         <MdOutlineShoppingCart className="text-2xl" />
       </Link>
-      </header>
+    </header>
   );
 };
 
