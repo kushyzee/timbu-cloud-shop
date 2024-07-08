ee5e62263146b9e6d00a346baaf93b0d5bc5d226
niklaus.oa <niklaus.oa@gmail.com>
2024-07-08 07:06:01 +0000

    move productList to assets

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import Hero from "../components/Hero";
+import Products from "../components/Products";
 const Home = () => {
   return (
     <main className="text-base text-customGray-800">
       <Hero />
+      <Products />
     </main>
   );
+
 };
 
 export default Home;
