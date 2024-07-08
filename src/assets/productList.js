4c2fdfd7f4b0c163f164431f7e726ba2922330a6
Arinze Ogbuniba <niklaus.oa@gmail.com>
2024-07-08 12:16:35 +0100

    Import all image

--- src/assets/productList.js
+++ src/assets/productList.js
@@ -1,10 +1,31 @@
 import classicAbMug from "./images/Classic AB mug.png";
+import redHeartWhiteMug from "./images/Red heart white mug.png";
+import coolBrownMug from "./images/Cool brown mug.png";
+import peachMug from "./images/Peach mug.png";
+import happyYellowMug from "./images/Happy yellow mug.png";
+import yellowOrangeMug from "./images/Yellow & orange mug.png";
+import blueMondayMug from "./images/Blue Monday mug.png";
+import smileyFaceMug from "./images/Smiley face mug.png";
+import plainWhiteMug from "./images/Plain white mug.png";
+import bestBossEverMug from "./images/“Best boss ever” mug.png";
+import whiteMetalMug from "./images/White metal mug.png";
+import winkyFaceMug from "./images/Winky face mug.png";
+import breakfastMug from "./images/Breakfast mug.png";
+import blackWideMug from "./images/Black wide mug.png";
+import worldsBestBossMug from "./images/World’s best boss.png";
+import slimYellowMug from "./images/Slim yellow mug.png";
+import theBossMug from "./images/The Boss mug.png";
+import blueCoffeeMug from "./images/Blue coffee mug.png";
+import whiteWideLipMug from "./images/White wide lip mug.png";
+import whiteWideMug from "./images/White wide mug.png";
+
+
 
 const products = [
   {
     id: 1,
     name: "Classic Ab mug",
-    price: "7,499.00",
+    price: "7,499.99",
     rating: "4.2",
     image: classicAbMug,
   },
@@ -13,133 +34,133 @@ const products = [
     name: "Red heart white mug",
     price: "6,500.00",
     rating: "4.1",
-    image: null,
+    image: redHeartWhiteMug,
   },
   {
     id: 3,
     name: "Cool brown mug",
     price: "5,500.00",
     rating: "4.5",
-    image: null,
+    image: coolBrownMug,
   },
   {
     id: 4,
     name: "Peach mug",
     price: "7,500.00",
     rating: "4.9",
-    image: null,
+    image: peachMug,
   },
   {
     id: 5,
     name: "Happy yellow mug",
     price: "7,000.00",
     rating: "4.5",
-    image: null,
+    image: happyYellowMug,
   },
   {
     id: 6,
     name: "Yellow & orange mug",
     price: "8,000.00",
     rating: "4.8",
-    image: null,
+    image: yellowOrangeMug,
   },
   {
     id: 7,
     name: "Blue Monday mug",
     price: "9,000.00",
     rating: "4.0",
-    image: null,
+    image: blueMondayMug,
   },
   {
     id: 8,
     name: "Smiley face mug",
     price: "8,000.00",
     rating: "4.4",
-    image: null,
+    image: smileyFaceMug,
   },
   {
     id: 9,
     name: "Plain white mug",
     price: "4,000.00",
     rating: "4.5",
-    image: null,
+    image: plainWhiteMug,
   },
   {
     id: 10,
     name: "“Best boss ever” mug",
     price: "6,000.00",
     rating: "4.1",
-    image: null,
+    image: bestBossEverMug,
   },
   {
     id: 11,
     name: "White metal mug",
     price: "5,500.00",
     rating: "4.1",
-    image: null,
+    image: whiteMetalMug,
   },
   {
     id: 12,
     name: "Winky face mug",
     price: "6,500.00",
     rating: "4.2",
-    image: null,
+    image: winkyFaceMug,
   },
   {
     id: 13,
     name: "Breakfast mug",
     price: "4,500.00",
     rating: "3.8",
-    image: null,
+    image: breakfastMug,
   },
   {
     id: 14,
     name: "Black wide mug",
     price: "4,000.00",
     rating: "3.9",
-    image: null,
+    image: blackWideMug,
   },
   {
     id: 15,
     name: "World’s best boss",
     price: "5,000.00",
     rating: "4.0",
-    image: null,
+    image: worldsBestBossMug,
   },
   {
     id: 16,
     name: "Slim yellow mug",
     price: "6,000.00",
     rating: "3.6",
-    image: null,
+    image: slimYellowMug,
   },
   {
     id: 17,
     name: "The Boss mug",
     price: "3,000.00",
     rating: "3.6",
-    image: null,
+    image: theBossMug,
   },
   {
     id: 18,
     name: "Blue coffee mug",
     price: "6,800.00",
     rating: "3.7",
-    image: null,
+    image: blueCoffeeMug,
   },
   {
     id: 19,
     name: "White wide lip mug",
     price: "6,200.00",
     rating: "3.8",
-    image: null,
+    image: whiteWideLipMug,
   },
   {
     id: 20,
     name: "White wide mug",
     price: "6,400.00",
     rating: "4.8",
-    image: null,
+    image: whiteWideMug,
   }
 ];
 
