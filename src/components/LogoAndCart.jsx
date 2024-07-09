59711f8480422033f4c0a89a82865fd8375237fc
Arinze Ogbuniba <82098877+kushyzee@users.noreply.github.com>
2024-07-09 17:33:05 +0000

    Initial commit

---
+++ src/components/LogoAndCart.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import { MdOutlineShoppingCart } from "react-icons/md";
+
+const LogoAndCart = () => {
+return <div className="mx-auto flex w-full max-w-screen-xl items-center justify-center md:justify-between">
+  <Link
+    to="/"
+    className="mx-auto inline-block self-center text-sm font-bold md:mx-0 lg:text-2xl"
+  >
+    TimbuCloudShop
+  </Link>
+  <Link to="cart" className="inline-flex items-center gap-2">
+    <MdOutlineShoppingCart className="text-2xl lg:text-3xl" />
+    <span className="hidden text-xl lg:inline-block font-semibold">Cart</span>
+  </Link>
+</div>
+}
+
+export default LogoAndCart
