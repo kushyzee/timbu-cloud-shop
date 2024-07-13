24422a03ef65b6df94a2cff3b9a56c68b4bdd129
Arinze Ogbuniba <niklaus.oa@gmail.com>
2024-07-13 18:01:02 +0100

    Add logic to get all products and product

--- src/apiService.js
+++ src/apiService.js
@@ -1,27 +1,44 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = "/api";
 const API_KEY = import.meta.env.VITE_API_KEY;
 const ORGANIZATION_ID = import.meta.env.VITE_ORGANIZATION_ID;
 const APP_ID = import.meta.env.VITE_APP_ID;
 
-const getProducts = async (page = 1, size = 20) => {
+const sendGetRequests = async (url, parameters) => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/products`, {
-      params: {
-        organization_id: ORGANIZATION_ID,
-        reverse_sort: false,
-        page,
-        size,
-        Appid: APP_ID,
-        Apikey: API_KEY,
-      },
+    const response = await axios.get(url, {
+      params: parameters,
     });
-    return response.data;
+    return response;
   } catch (error) {
     throw new Error(error);
   }
 };
-getProducts();
+
+const getProducts = async (page = 1) => {
+  const url = `${API_BASE_URL}/products`;
+  const params = {
+    organization_id: ORGANIZATION_ID,
+    reverse_sort: false,
+    page: page,
+    size: 30,
+    Appid: APP_ID,
+    Apikey: API_KEY,
+  };
+  const response = await sendGetRequests(url, params);
+  return response.data;
+};
+
+export const getProduct = async (productID) => {
+  const url = `${API_BASE_URL}/products/${productID}`;
+  const params = {
+    organization_id: ORGANIZATION_ID,
+    Appid: APP_ID,
+    Apikey: API_KEY,
+  };
+  const response = await sendGetRequests(url, params);
+  return response.data
+};
 
 export default getProducts;
