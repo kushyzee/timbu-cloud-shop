a3f36d18e6d2cdbfb2a145910de98517dea7670b
Arinze Ogbuniba <niklaus.oa@gmail.com>
2024-07-14 19:32:06 +0100

    Collect sales details on submit

--- src/components/ShippingAddress.jsx
+++ src/components/ShippingAddress.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { useState } from "react";
 
-const ShippingAddress = () => {
+const ShippingAddress = ({ customerDetails, handleChange }) => {
   return (
     <div>
       <h2 className="mb-3 text-base font-semibold md:text-xl">
@@ -8,22 +9,31 @@ const ShippingAddress = () => {
       </h2>
       <div className="grid gap-3 md:gap-5">
         <div>
-          <label htmlFor="name" className="mb-1 block text-sm md:text-base font-medium">
+          <label
+            htmlFor="name"
+            className="mb-1 block text-sm font-medium md:text-base"
+          >
             Name
           </label>
           <div>
             <input
               required
+              name="name"
               type="text"
               id="name"
               autoComplete="name"
+              value={customerDetails.name}
+              onChange={handleChange}
               className="w-full rounded-md border-2 border-customGray-800 p-2.5 text-sm focus:border-customGray-600 focus:outline-none focus:ring-1 focus:ring-customGray-600"
             />
           </div>
         </div>
 
         <div>
-          <label htmlFor="address" className="mb-1 block text-sm md:text-base font-medium">
+          <label
+            htmlFor="address"
+            className="mb-1 block text-sm font-medium md:text-base"
+          >
             Address
           </label>
           <div>
@@ -38,22 +48,52 @@ const ShippingAddress = () => {
         </div>
 
         <div>
-          <label htmlFor="phone" className="mb-1 block text-sm md:text-base font-medium">
+          <label
+            htmlFor="phone"
+            className="mb-1 block text-sm font-medium md:text-base"
+          >
             Phone number
           </label>
           <div>
             <input
               required
+              name="phone"
               type="tel"
               id="phone"
               autoComplete="tel"
+              value={customerDetails.phone}
+              onChange={handleChange}
               className="w-full rounded-md border-2 border-customGray-800 p-2.5 text-sm focus:border-customGray-600 focus:outline-none focus:ring-1 focus:ring-customGray-600"
             />
           </div>
         </div>
 
         <div>
-          <label htmlFor="city" className="mb-1 block text-sm md:text-base font-medium">
+          <label
+            htmlFor="email"
+            className="mb-1 block text-sm font-medium md:text-base"
+          >
+            Email
+          </label>
+          <div>
+            <input
+              required
+              name="email"
+              type="tel"
+              id="email"
+              autoComplete="email"
+              value={customerDetails.email}
+              onChange={handleChange}
+              className="w-full rounded-md border-2 border-customGray-800 p-2.5 text-sm focus:border-customGray-600 focus:outline-none focus:ring-1 focus:ring-customGray-600"
+            />
+          </div>
+        </div>
+
+        <div>
+          <label
+            htmlFor="city"
+            className="mb-1 block text-sm font-medium md:text-base"
+          >
             City
           </label>
           <div>
@@ -68,7 +108,10 @@ const ShippingAddress = () => {
         </div>
 
         <div>
-          <label htmlFor="state" className="mb-1 block text-sm md:text-base font-medium">
+          <label
+            htmlFor="state"
+            className="mb-1 block text-sm font-medium md:text-base"
+          >
             State
           </label>
           <div>
@@ -83,7 +126,10 @@ const ShippingAddress = () => {
         </div>
 
         <div>
-          <label htmlFor="zip" className="mb-1 block text-sm md:text-base font-medium">
+          <label
+            htmlFor="zip"
+            className="mb-1 block text-sm font-medium md:text-base"
+          >
             Zip
           </label>
           <div>
